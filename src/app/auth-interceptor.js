6294b300372a6f421b07807e8cbd6961556a7f0c
omer.farooq <omer.farooq@10pearls.local>
2018-06-11 17:24:21 +0500

    response interceptor started

---
+++ src/app/auth-interceptor.js
@@ -0,0 +1,31 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+exports.__esModule = true;
+var core_1 = require("@angular/core");
+var AuthInterceptor = /** @class */ (function () {
+    function AuthInterceptor() {
+    }
+    AuthInterceptor.prototype.intercept = function (req, next) {
+        var idToken = localStorage.getItem('id_token');
+        if (idToken) {
+            var cloned = req.clone({
+                headers: req.headers.set('Authorization', idToken)
+            });
+            return next.handle(cloned);
+        }
+        else {
+            console.log('JWT not found');
+            return next.handle(req);
+        }
+    };
+    AuthInterceptor = __decorate([
+        core_1.Injectable()
+    ], AuthInterceptor);
+    return AuthInterceptor;
+}());
+exports.AuthInterceptor = AuthInterceptor;
